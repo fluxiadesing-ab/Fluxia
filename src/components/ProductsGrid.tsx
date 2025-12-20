8f0ec43c813618a4ef866c6d630fbd2d2204522e
fluxiadesing-ab <fluxiadesign@gmail.com>
2025-12-20 09:10:40 +0300

    rrr

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -178,11 +178,11 @@ const ProductsGrid: React.FC = () => {
 
 
         {/* Products Grid */}
-        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-6">
+        {/* <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-6">
           {products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-        </div>
+        </div> */}
       </div>
     </section>
   );
