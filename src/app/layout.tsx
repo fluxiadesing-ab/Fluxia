45d3f148bb8b08942b0c276898633078c7efe974
fluxiadesing-ab <fluxiadesing@gmail.com>
2026-03-03 22:32:15 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,27 +78,26 @@ export default function RootLayout({
             });
           `}
         </Script>
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-  !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-
-  fbq('init', '4279361315612515');
-  fbq('track', 'PageView');
-`}
-        </Script>
+<Script id="meta-pixel" strategy="beforeInteractive">
+  {`
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '4279361315612515');
+    fbq('track', 'PageView');
+  `}
+</Script>
         <noscript>
           <img
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=PIXEL_ID_HERE&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=4279361315612515&ev=PageView&noscript=1"
           />
         </noscript>
         <link rel="icon" href="/favicon.ico" />
