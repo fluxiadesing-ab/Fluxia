bda9f0ae317a3a0332c4fd7e7c31febd761d5e5f
fluxiadesing-ab <fluxiadesing@gmail.com>
2026-03-03 21:28:04 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { Roboto_Condensed } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
-
-
+import MetaPixelTracker from "../components/MetaPixelTracker";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,25 +24,27 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   // العنوان: جعلناه أشمل ليشمل المنتجات الأخرى
   title: "Fluxia | Custom Printing on T-Shirts, Mugs, Bags & More",
-  
+
   // الوصف: ركزنا هنا على التنوع (جاهز + مخصص) وعلى قائمة المنتجات
-  description: "Create your style with Fluxia. High-quality custom printing on t-shirts, hoodies, mugs, and bags. Choose from our ready-made collections or upload your own design today!",
-  
+  description:
+    "Create your style with Fluxia. High-quality custom printing on t-shirts, hoodies, mugs, and bags. Choose from our ready-made collections or upload your own design today!",
+
   // الكلمات المفتاحية: أضفنا المنتجات الجديدة
   keywords: [
-    "custom t-shirts", 
-    "custom hoodies", 
-    "personalized mugs", 
-    "custom tote bags", 
-    "print on demand", 
-    "ready-made designs", 
-    "Fluxia store"
+    "custom t-shirts",
+    "custom hoodies",
+    "personalized mugs",
+    "custom tote bags",
+    "print on demand",
+    "ready-made designs",
+    "Fluxia store",
   ],
 
   // تحسين الظهور عند مشاركة الرابط (OpenGraph)
   openGraph: {
     title: "Fluxia | Your Creative Printing Store",
-    description: "Personalize your world! Custom and ready-made designs for t-shirts, mugs, bags, and hoodies.",
+    description:
+      "Personalize your world! Custom and ready-made designs for t-shirts, mugs, bags, and hoodies.",
     url: "https://fluxiadesign.com",
     siteName: "Fluxia",
     locale: "en_US",
@@ -77,6 +78,29 @@ export default function RootLayout({
             });
           `}
         </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+
+  fbq('init', '4279361315612515');
+  fbq('track', 'PageView');
+`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=PIXEL_ID_HERE&ev=PageView&noscript=1"
+          />
+        </noscript>
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body
@@ -85,6 +109,7 @@ export default function RootLayout({
     antialiased
   `}
       >
+        <MetaPixelTracker />
         {children}
       </body>
     </html>
